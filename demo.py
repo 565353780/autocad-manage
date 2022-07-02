884b3ee91805eed60e4e7763f422f180f2ef517b
chLi <565353780@qq.com>
2022-07-02 14:39:31 +0800

    simplify demo

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Module.dxf_layout_detector import demo as layout_demo
+from Module.dxf_layout_detector import demo
 
 if __name__ == "__main__":
-    layout_demo()
+    demo()
 
