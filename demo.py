8bd3c7e23d7e9c1d5169d6c228fbfa67b5370622
chLi <565353780@qq.com>
2022-07-02 14:39:10 +0800

    simplify demo code

--- demo.py
+++ demo.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Module.dxf_loader import demo as loader_demo
-from Module.dxf_renderer import demo as render_demo
 from Module.dxf_layout_detector import demo as layout_demo
 
 if __name__ == "__main__":
-    #  loader_demo()
-    #  render_demo()
     layout_demo()
 
