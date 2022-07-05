09e14b54f564ec670c96116c766a100b37a617ae
chLi <565353780@qq.com>
2022-07-05 20:28:38 +0800

    update config param name

--- Config/base_config.py
+++ Config/base_config.py
@@ -5,7 +5,7 @@ BASE_CONFIG = {
     'dxf_file_path': 'FIXME',
     'debug': True,
     'dist_error_max': 0.1,
-    'length_error_max': 10,
+    'length_error_ratio_max': 0.1,
     'angle_error_max': 5,
     'k_0_max': 1e-6,
     'k_inf_min': 1e6,
