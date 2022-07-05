1eedeb0ae64154b39e67a69b9e530827a5e55792
chLi <565353780@qq.com>
2022-07-05 19:11:51 +0800

    simplify configs

---
+++ Config/base_config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+BASE_CONFIG = {
+    'dxf_file_path': 'FIXME',
+    'debug': True,
+    'dist_error_max': 0.1,
+    'length_error_max': 10,
+    'angle_error_max': 5,
+    'k_0_max': 1e-6,
+    'k_inf_min': 1e6,
+    'image_width': 1600 * 1.2,
+    'image_height': 900 * 1.2,
+    'free_width': 50,
+    'is_reverse_y': True,
+    'window_name': 'Render Result',
+}
+
--- Config/configs.py
+++ Config/configs.py
@@ -1,12 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Config.render_all import RENDER_ALL
-from Config.layout_test1 import LAYOUT_TEST1
-from Config.layout_3 import LAYOUT_3
-from Config.layout_4 import LAYOUT_4
-from Config.layout_5 import LAYOUT_5
-from Config.layout_6 import LAYOUT_6
-from Config.layout_9 import LAYOUT_9
-from Config.layout_10 import LAYOUT_10
+from copy import deepcopy
+
+from Config.base_config import BASE_CONFIG
+from Config.dxf_paths import DXF_DICT, VALID_KEY_LIST
+
+RENDER_ALL = deepcopy(BASE_CONFIG)
+RENDER_ALL['dxf_file_path'] = DXF_DICT[VALID_KEY_LIST[0]]
+RENDER_ALL['window_name'] = 'Render all'
+
+LAYOUT_TEST1 = deepcopy(BASE_CONFIG)
+LAYOUT_TEST1['dxf_file_path'] = DXF_DICT['test1']
+LAYOUT_TEST1['window_name'] = 'Layout test1'
+
+LAYOUT_3 = deepcopy(BASE_CONFIG)
+LAYOUT_3['dxf_file_path'] = DXF_DICT['3']
+LAYOUT_3['window_name'] = 'Layout 3'
+
+LAYOUT_4 = deepcopy(BASE_CONFIG)
+LAYOUT_4['dxf_file_path'] = DXF_DICT['4']
+LAYOUT_4['window_name'] = 'Layout 4'
+
+LAYOUT_5 = deepcopy(BASE_CONFIG)
+LAYOUT_5['dxf_file_path'] = DXF_DICT['5']
+LAYOUT_5['window_name'] = 'Layout 5'
+
+LAYOUT_6 = deepcopy(BASE_CONFIG)
+LAYOUT_6['dxf_file_path'] = DXF_DICT['6']
+LAYOUT_6['window_name'] = 'Layout 6'
+
+LAYOUT_9 = deepcopy(BASE_CONFIG)
+LAYOUT_9['dxf_file_path'] = DXF_DICT['9']
+LAYOUT_9['window_name'] = 'Layout 9'
+
+LAYOUT_10 = deepcopy(BASE_CONFIG)
+LAYOUT_10['dxf_file_path'] = DXF_DICT['10']
+LAYOUT_10['window_name'] = 'Layout 10'
+
+CONFIG_COLLECTION = {
+    'render_all': RENDER_ALL,
+    'test1': LAYOUT_TEST1,
+    '3': LAYOUT_3,
+    '4': LAYOUT_4,
+    '5': LAYOUT_5,
+    '6': LAYOUT_6,
+    '9': LAYOUT_9,
+    '10': LAYOUT_10,
+}
 
