83456fc1892628ef3079b6e51e1b2dba6f194140
chLi <565353780@qq.com>
2022-08-27 16:51:39 +0800

    finish basic load file algo

--- autocad_manage/Module/dwg_loader.py
+++ autocad_manage/Module/dwg_loader.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import win32com.client
+#  import win32com.client
 import comtypes.client
-
-try:
-        acad = comtypes.client.GetActiveObject("AutoCAD.Application")
-except:
-        acad = comtypes.client.CreateObject("AutoCAD.Application")
-
-acad.Visible = True
-doc = acad.Documents.Open("C:\\Users\\Administrator\\Desktop\\tulesheng.dwg")
 
 class DWGLoader(object):
     def __init__(self):
+        self.autocad = None
+
+        self.doc = None
+
+        self.connectAutoCAD()
         return
 
+    def connectAutoCAD(self):
+        try:
+            self.autocad = comtypes.client.GetActiveObject("AutoCAD.Application")
+        except:
+            print("[ERROR][DWGLoader::connectAutoCAD]")
+            print("\t GetActiveObject failed!")
+            return False
+
+        self.autocad.Visible = True
+        return True
+
+    def openDWGFile(self, dwg_file_path):
+        try:
+            self.doc = self.autocad.Documents.Open(dwg_file_path)
+        except:
+            print("[ERROR][DWGLoader::openDWGFile]")
+            print("\t Open failed!")
+            return False
+        return True
+
 def demo():
+    dwg_file_path = "L:/"
     dwg_loader = DWGLoader()
+    dwg_loader.openDWGFile(dwg_file_path)
     return True
 
