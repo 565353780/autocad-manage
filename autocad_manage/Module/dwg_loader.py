bc762d8aea047948c26bfd9872bb285d2619c68a
chLi <565353780@qq.com>
2022-08-27 22:39:33 +0800

    finish auto load dwg files and close; need to saveas dwg again to skip manual op

--- autocad_manage/Module/dwg_loader.py
+++ autocad_manage/Module/dwg_loader.py
@@ -4,6 +4,7 @@
 import os
 import comtypes.client
 #  import win32com.client
+from tqdm import tqdm
 
 from autocad_manage.Method.path import createFileFolder
 
@@ -34,8 +35,21 @@ class DWGLoader(object):
         return True
 
     def sendCMD(self, cmd):
-        doc = self.autocad.activedocument
-        doc.SendCommand(cmd)
+        doc = None
+        try:
+            doc = self.autocad.activedocument
+        except:
+            print("[ERROR][DWGLoader::sendCMD]")
+            print("\t select activedocument failed!")
+            return False
+
+        try:
+            doc.SendCommand(cmd)
+        except:
+            print("[ERROR][DWGLoader::sendCMD]")
+            print("\t SendCommand failed!")
+            print("\t", cmd)
+            return False
         return True
 
     def openDWGFile(self, dwg_file_path):
@@ -58,14 +72,28 @@ class DWGLoader(object):
             return False
 
         cmd = "FILEDIA " + "0 "
-        self.sendCMD(cmd)
+        if not self.sendCMD(cmd):
+            print("[ERROR][DWGLoader::openDWGFile]")
+            print("\t set FILEDIA to 0 failed!")
+            return False
         return True
 
     def saveAs(self, save_file_path):
         createFileFolder(save_file_path)
 
-        cmd = "SAVEAS " + "dxf " + "16 " + save_file_path + " "
-        self.sendCMD(cmd)
+        cmd = "SAVEAS " + "dxf " + "16 " + save_file_path + "\n"
+        if not self.sendCMD(cmd):
+            print("[ERROR][DWGLoader::saveAs]")
+            print("\t sendCMD failed!")
+            return False
+        return True
+
+    def closeDoc(self):
+        cmd = "CLOSE "
+        if not self.sendCMD(cmd):
+            print("[ERROR][DWGLoader::closeDoc]")
+            print("\t sendCMD failed!")
+            return False
         return True
 
 def demo():
@@ -75,9 +103,9 @@ def demo():
         "L:/CAD/DXF/House_1/20210223/C00000103/1e9a1ce8f2104613bfa4882cff473c42.dxf"
 
     dwg_file_path = \
-        "/home/chli/chLi/CAD/户型识别文件/1.dwg"
+        "L:/CAD/户型识别文件/1.dwg"
     save_file_path = \
-        "/home/chli/chLi/1.dxf"
+        "L:/1.dxf"
 
     dwg_loader = DWGLoader()
     dwg_loader.openDWGFile(dwg_file_path)
@@ -85,19 +113,45 @@ def demo():
     return True
 
 def demo_folder():
-    dwg_folder_path = "L:/CAD/"
-    save_folder_path = "L:/CAD/DXF/"
+    dwg_folder_path = "L:/CAD/户型识别文件/"
+    save_folder_path = "L:/CAD/DXF/户型识别文件/"
 
     dwg_loader = DWGLoader()
+
+    file_path_pair_list = []
     for root, _, files in os.walk(dwg_folder_path):
         for file_name in files:
             if file_name[-4:] != ".dwg":
                 continue
-            dwg_file_path = root + "/" + file_name
+            dwg_file_path = root + file_name
             save_file_path = \
-                root.replace(dwg_folder_path, save_folder_path) + "/" + \
+                root.replace(dwg_folder_path, save_folder_path) + \
                 file_name[:-4] + ".dxf"
-            dwg_loader.openDWGFile(dwg_file_path)
-            dwg_loader.saveAs(save_file_path)
+            file_path_pair_list.append([dwg_file_path, save_file_path])
+
+    print("[INFO][DWGLoader::demo_folder]")
+    print("\t start save dxf files...")
+    for file_path_pair in tqdm(file_path_pair_list):
+        dwg_file_path, save_file_path = file_path_pair
+
+        if os.path.exists(save_file_path):
+            continue
+
+        tmp_file_path = save_file_path[:-4] + "_tmp.dxf"
+
+        dwg_loader.openDWGFile(dwg_file_path)
+        dwg_loader.saveAs(tmp_file_path)
+        dwg_loader.closeDoc()
+
+        if not os.path.exists(tmp_file_path):
+            print("[ERROR][DWGLoader::demo_folder]")
+            print("\t save dxf file failed!")
+            return False
+
+        while os.path.exists(tmp_file_path):
+            try:
+                os.rename(tmp_file_path, save_file_path)
+            except:
+                continue
     return True
 
