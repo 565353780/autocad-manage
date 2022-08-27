5ad5e364c1233e07f04dcc2d3b742c17943952d5
chLi <565353780@qq.com>
2022-08-27 14:11:43 +0800

    create basic dwg loader class

---
+++ autocad_manage/Module/dwg_loader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class DWGLoader(object):
+    def __init__(self):
+        return
+
+def demo():
+    dwg_loader = DWGLoader()
+    return True
+
--- demo.py
+++ demo.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from autocad_manage.Module.dxf_loader import demo as load_demo
+from autocad_manage.Module.dwg_loader import demo as dwg_load_demo
+from autocad_manage.Module.dxf_loader import demo as dxf_load_demo
 from autocad_manage.Module.dxf_layout_detector import \
     demo as layout_detect_demo, \
     demo_debug as layout_detect_debug_demo
 
 if __name__ == "__main__":
-    load_demo()
+    dwg_load_demo()
+    #  dxf_load_demo()
     #  layout_detect_demo()
     #  layout_detect_debug_demo()
 
