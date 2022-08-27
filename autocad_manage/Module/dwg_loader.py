a39e1ab31168be5722514460c04e4e0805011208
chLi <565353780@qq.com>
2022-08-27 16:26:52 +0800

    add basic test

--- autocad_manage/Module/dwg_loader.py
+++ autocad_manage/Module/dwg_loader.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import win32com.client
+import comtypes.client
+
+try:
+        acad = comtypes.client.GetActiveObject("AutoCAD.Application")
+except:
+        acad = comtypes.client.CreateObject("AutoCAD.Application")
+
+acad.Visible = True
+doc = acad.Documents.Open("C:\\Users\\Administrator\\Desktop\\tulesheng.dwg")
+
 class DWGLoader(object):
     def __init__(self):
         return
