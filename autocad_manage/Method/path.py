a4ab8f05d935cad9bef333908e17cc9db32ad1ed
chLi <565353780@qq.com>
2022-08-28 13:51:59 +0800

    add rename func

--- autocad_manage/Method/path.py
+++ autocad_manage/Method/path.py
@@ -19,3 +19,16 @@ def removeIfExist(file_path):
             continue
     return True
 
+def renameFile(file_path, target_file_path):
+    if not os.path.exists(file_path):
+        print("[ERROR][path::renameFile]")
+        print("\t file not exist!")
+        return False
+
+    while os.path.exists(file_path):
+        try:
+            os.rename(file_path, target_file_path)
+        except:
+            continue
+    return True
+
