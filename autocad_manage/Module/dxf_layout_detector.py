e69aea0f30a64a4337e061f088c740d20434f8f6
chLi <565353780@qq.com>
2022-09-18 18:11:11 +0800

    add compare_with_all_shape option param

--- autocad_manage/Module/dxf_layout_detector.py
+++ autocad_manage/Module/dxf_layout_detector.py
@@ -661,7 +661,8 @@ class DXFLayoutDetector(DXFRenderer):
     def transDxfToJsonWithLayout(self,
                                  dxf_file_path,
                                  save_json_file_path,
-                                 save_image_file_path=None):
+                                 save_image_file_path=None,
+                                 compare_with_all_shape=False):
         if not self.loadFile(dxf_file_path):
             print("[ERROR][DXFLayoutDetector::transDxfToJsonWithLayout]")
             print("\t loadFile failed!")
@@ -675,7 +676,7 @@ class DXFLayoutDetector(DXFRenderer):
             return False
 
         if save_image_file_path is not None:
-            if not self.saveImage(save_image_file_path):
+            if not self.saveImage(save_image_file_path, compare_with_all_shape):
                 print("[ERROR][DXFLayoutDetector::transDxfToJsonWithLayout]")
                 print("\t saveImage failed!")
                 print("\t", save_image_file_path)
@@ -708,20 +709,21 @@ class DXFLayoutDetector(DXFRenderer):
                                        dxf_folder_path,
                                        save_json_folder_path,
                                        save_image_folder_path=None,
+                                       compare_with_all_shape=False,
                                        print_progress=False):
         file_path_pair_list = []
         for root, _, files in os.walk(dxf_folder_path):
             for file_name in files:
                 if file_name[-4:] != ".dxf":
                     continue
-                dxf_file_path = root + file_name
+                dxf_file_path = root + "/" + file_name
                 save_json_file_path = \
-                    root.replace(dxf_folder_path, save_json_folder_path) + \
+                    root.replace(dxf_folder_path, save_json_folder_path) + "/" + \
                     file_name[:-4] + ".json"
                 save_image_file_path = None
                 if save_image_folder_path is not None:
                     save_image_file_path = \
-                        root.replace(dxf_folder_path, save_image_folder_path) + \
+                        root.replace(dxf_folder_path, save_image_folder_path) + "/" + \
                         file_name[:-4] + ".png"
                 file_path_pair_list.append([dxf_file_path, save_json_file_path, save_image_file_path])
 
@@ -750,7 +752,7 @@ class DXFLayoutDetector(DXFRenderer):
                 return False
 
             if save_image_file_path is not None:
-                if not self.saveImage(save_image_file_path):
+                if not self.saveImage(save_image_file_path, compare_with_all_shape):
                     print("[INFO][DXFLayoutDetector::transDxfFolderToJsonWithLayout]")
                     print("\t saveImage failed!")
                     print("\t", save_image_file_path)
@@ -777,10 +779,11 @@ def demo():
     dxf_file_path = "/home/chli/chLi/CAD/DXF/户型识别文件/1.dxf"
     save_json_file_path = "/home/chli/chLi/CAD/JSON/户型识别文件/1.json"
     save_image_file_path = "/home/chli/chLi/CAD/Image/户型识别文件/1.png"
+    compare_with_all_shape = True
 
     dxf_layout_detector = DXFLayoutDetector(dxf_file_path)
     dxf_layout_detector.outputInfo()
-    dxf_layout_detector.saveImage(save_image_file_path)
+    dxf_layout_detector.saveImage(save_image_file_path, compare_with_all_shape)
     dxf_layout_detector.render()
     waitKey(0)
     dxf_layout_detector.saveJson(save_json_file_path)
@@ -790,22 +793,31 @@ def demo_trans():
     dxf_file_path = "/home/chli/chLi/CAD/DXF/户型识别文件/1.dxf"
     save_json_file_path = "/home/chli/chLi/CAD/JSON/户型识别文件/1.json"
     save_image_file_path = "/home/chli/chLi/CAD/Image/户型识别文件/1.png"
+    compare_with_all_shape = True
 
     dxf_layout_detector = DXFLayoutDetector()
-    dxf_layout_detector.transDxfToJsonWithLayout(dxf_file_path,
-                                                 save_json_file_path,
-                                                 save_image_file_path)
+    dxf_layout_detector.transDxfToJsonWithLayout(
+        dxf_file_path,
+        save_json_file_path,
+        save_image_file_path,
+        compare_with_all_shape
+    )
     return True
 
 def demo_trans_folder():
-    dxf_folder_path = "/home/chli/chLi/CAD/DXF/户型识别文件/"
-    save_json_folder_path = "/home/chli/chLi/CAD/JSON/户型识别文件/"
-    save_image_folder_path = "/home/chli/chLi/CAD/Image/户型识别文件/"
+    dxf_folder_path = "/home/chli/chLi/CAD/DXF/House_1/"
+    save_json_folder_path = "/home/chli/chLi/CAD/JSON/House_1/"
+    save_image_folder_path = "/home/chli/chLi/CAD/Image/House_1/"
+    compare_with_all_shape = True
+    print_progress = True
 
     dxf_layout_detector = DXFLayoutDetector()
-    dxf_layout_detector.transDxfFolderToJsonWithLayout(dxf_folder_path,
-                                                       save_json_folder_path,
-                                                       save_image_folder_path,
-                                                       True)
+    dxf_layout_detector.transDxfFolderToJsonWithLayout(
+        dxf_folder_path,
+        save_json_folder_path,
+        save_image_folder_path,
+        compare_with_all_shape,
+        print_progress
+    )
     return True
 
