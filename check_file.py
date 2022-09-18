cd31d78f1f7a029e538e13d145f89d412979e50f
chLi <565353780@qq.com>
2022-09-19 00:02:34 +0800

    add some useful output info

--- check_file.py
+++ check_file.py
@@ -32,9 +32,13 @@ print(len(source_file_path_list))
 print(len(target_file_name_list))
 print(len(move_file_path_list))
 
-for source_file_path in move_file_path_list:
-    source_file_name = source_file_path.split("/")[-1]
-    copyfile(source_file_path, move_folder_path + source_file_name)
+#  copy_num = 0
+#  for source_file_path in move_file_path_list:
+    #  source_file_name = source_file_path.split("/")[-1]
+    #  copyfile(source_file_path, move_folder_path + source_file_name)
+    #  copy_num += 1
+    #  if copy_num >= 20:
+        #  break
 
 def isSameList(list_1, list_2):
     if len(list_1) != len(list_2):
@@ -44,12 +48,28 @@ def isSameList(list_1, list_2):
             return False
     return True
 
+total_num = 0
+
+first_label_count_list = []
+
 target_file_label_count_list = []
 for root, _, files in os.walk(target_folder_path):
     for file_name in files:
         if file_name[-4:] != ".png":
             continue
         file_label_list = root.split(target_folder_path)[1].split("/")
+
+        total_num += 1
+
+        find_exist_label = False
+        for i in range(len(first_label_count_list)):
+            if first_label_count_list[i][0] == file_label_list[0]:
+                find_exist_label = True
+                first_label_count_list[i][1] += 1
+                break
+
+        if not find_exist_label:
+            first_label_count_list.append([file_label_list[0], 1])
 
         find_exist_label = False
         for i in range(len(target_file_label_count_list)):
@@ -58,17 +78,28 @@ for root, _, files in os.walk(target_folder_path):
                 target_file_label_count_list[i][1] += 1
                 break
 
-        if find_exist_label:
-            continue
+        if not find_exist_label:
+            target_file_label_count_list.append([file_label_list, 1])
 
-        target_file_label_count_list.append([file_label_list, 1])
+print()
+print("Total dxf layout detect num = " + str(total_num))
 
+print()
+for first_label_count in first_label_count_list:
+    first_label, count = first_label_count
+    percent = int(10000.0 * count / total_num) / 100.0
+
+    print(first_label + " : " + str(count) + " || " + str(percent) + "%")
+
+print()
 for target_file_label_count in target_file_label_count_list:
     target_file_label, count = target_file_label_count
+    percent = int(10000.0 * count / total_num) / 100.0
+
     for i in range(len(target_file_label)):
         if i == 0:
             print(target_file_label[i], end="")
             continue
         print(" -> " + target_file_label[i], end="")
-    print(" : " + str(count))
+    print(" : " + str(count) + " || " + str(percent) + "%")
 
