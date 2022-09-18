1bf6868d9032d82cf26863bed7b2145ccb2ae6b9
chLi <565353780@qq.com>
2022-09-18 20:12:59 +0800

    add isFinite func

--- autocad_manage/Data/point.py
+++ autocad_manage/Data/point.py
@@ -18,7 +18,13 @@ class Point(BaseShape):
             return True
         return False
 
-    def outputInfo(self, info_level):
+    def isFinite(self):
+        inf_list = [float("inf"), -float("inf")]
+        if self.x in inf_list or self.y in inf_list or self.z in inf_list:
+            return False
+        return True
+
+    def outputInfo(self, info_level=0):
         line_start = "\t" * info_level
         print(line_start + "[Point]")
         print(line_start + "\t [" + \
