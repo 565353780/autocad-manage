2c42a56a543705c34d0b6aba92a78e4824bc5b0d
chLi <565353780@qq.com>
2022-12-01 16:17:11 +0800

    update test files

--- autocad_manage/Demo/dxf_loader.py
+++ autocad_manage/Demo/dxf_loader.py
@@ -5,8 +5,8 @@ from autocad_manage.Module.dxf_loader import DXFLoader
 
 
 def demo():
-    dxf_file_path = "/home/chli/chLi/CAD/DXF/户型识别文件/1.dxf"
-    save_json_file_path = "/home/chli/chLi/CAD/JSON/户型识别文件/1.json"
+    dxf_file_path = "/home/chli/chLi/CAD/DXF/订单图纸/40mm层板生产详图+台面图用于时光木语2.dxf"
+    save_json_file_path = "/home/chli/chLi/CAD/JSON/订单图纸/40mm层板生产详图+台面图用于时光木语2.json"
 
     dxf_loader = DXFLoader(dxf_file_path)
     dxf_loader.outputInfo()
--- autocad_manage/Demo/dxf_renderer.py
+++ autocad_manage/Demo/dxf_renderer.py
@@ -7,13 +7,13 @@ from autocad_manage.Module.dxf_renderer import DXFRenderer
 
 
 def demo():
-    dxf_file_path = "/home/chli/chLi/CAD/DXF/户型识别文件/1.dxf"
-    save_image_file_path = "/home/chli/chLi/CAD/Image/户型识别文件/1.png"
+    dxf_file_path = "/home/chli/chLi/CAD/DXF/订单图纸/40mm层板生产详图+台面图用于时光木语2.dxf"
+    save_image_file_path = "/home/chli/chLi/CAD/Image/订单图纸/40mm层板生产详图+台面图用于时光木语2.png"
     compare_with_all_shape = True
 
     dxf_renderer = DXFRenderer(dxf_file_path)
     dxf_renderer.outputInfo()
     dxf_renderer.saveImage(save_image_file_path, compare_with_all_shape)
     dxf_renderer.render()
-    cv2.waitKey(0)
+    cv2.waitKey(10000)
     return True
